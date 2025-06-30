d0783e72942ec47854f322fd1321128db4a22229
Gayathri2803 <gayathribellapukonda28@gmail.com>
2025-06-30 18:32:42 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
 chrome.webNavigation.onCompleted.addListener(async ({ tabId, url }) => {
   try {
+    console.log("Navigation event detected:", url);
+
     const response = await fetch("https://raw.githubusercontent.com/Gayathri2803/phishing-list/main/phishing-list.json");
     const data = await response.json();
-    
+    console.log("Fetched phishing list:", data);
+
     const hostname = new URL(url).hostname;
+    console.log("Checking hostname:", hostname);
+
     const isPhishing = data.blacklist.includes(hostname);
+    console.log("Is phishing site?", isPhishing);
 
     if (isPhishing) {
       chrome.scripting.executeScript({
@@ -18,4 +24,3 @@ chrome.webNavigation.onCompleted.addListener(async ({ tabId, url }) => {
     console.error("Error checking phishing list:", err);
   }
 }, { url: [{ schemes: ["http", "https"] }] });
-
